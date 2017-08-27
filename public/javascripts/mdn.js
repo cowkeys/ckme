9ee325863025c32a4b2b65521a0598ec1684a865
liushunan <cowkeys@outlook.com>
2017-08-28 01:18:41 +0800

    init

---
+++ public/javascripts/mdn.js
@@ -0,0 +1,44 @@
+function addMsg(msg){
+    $.ajax({
+        url:"/chat/save",
+        type:"POST",
+        data:msg,
+        success:function(result){
+            //console.log("==post===",result);
+            getMsg();
+        }});
+}
+
+
+function getMsg(){
+    $.ajax({
+        url:"/chat/query",
+        type:"GET",
+        success:function(result){
+            //console.log("==get===",result);
+            showMsg(result);
+        }});
+}
+
+function showMsg(result){
+    for (var i = 0; i<result.length;i++){
+        $("#chatbox li").first().before("<li><p>"+result[i].time+"<p>"+result[i].content+"</li>");
+    }
+}
+
+function send(){
+    var content = $("#cnt").val();
+    if (!content){
+        return
+    }
+    var msg = {
+        content,
+    }
+    addMsg(msg);
+}
+
+$(function() {
+    getMsg();
+    console.log("written by lsn");
+    console.log("2017.08.28 love-day");
+});
---
+++ routes/chat.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var moment = require('moment');
+/* GET users listing. */
+var chat = [];
+router.post('/save', function(req, res, next) {
+    if (!req.body){
+        return res.send("failed!");
+    }
+    var msg = req.body;
+    if (!msg.content){
+        return res.send("content failed!");
+    }
+    msg.time = moment().format("YYYY-MM-DD hh:mm:ss");
+    chat.push(msg);
+    res.send('success!');
+});
+
+router.get('/query', function(req, res, next) {
+  res.send(chat);
+});
+
+module.exports = router;
