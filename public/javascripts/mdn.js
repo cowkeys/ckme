6968ad9d47a28352da6fa1f06b2b3dc4e10f56fd
cowkeys <cowkeys@outlook.com>
2019-03-05 15:11:03 +0800

    update pic

--- public/javascripts/mdn.js
+++ public/javascripts/mdn.js
@@ -11,6 +11,37 @@ function addMsg(msg){
         }});
 }
 
+function initimg(){
+    $("#upfile").change(function(evt){
+        // $filePath=URL.createObjectURL(this.files[0]);
+        // $('#preview').attr('src',$filePath);
+    console.log("ini")
+        var form = new FormData();
+        form.append("file", this.files[0]);
+        form.append("key", "mdn");
+        form.append("fileName", this.files[0].name);
+        form.append("bucketName", "cowkeys");
+    
+        var settings = {
+            async: true,
+            crossDomain: true,
+//            url: "https://api.maduanni.xn--6qq986b3xl/common/api/common/v1/file",
+            url: "http://api.maduanni.xn--6qq986b3xl/api/common/v1/file",
+            method: "post",
+            processData: false,
+            contentType: false,
+            mimeType: "multipart/form-data",
+            data: form
+        }
+        $.ajax(settings).done(function(response){
+            var data=JSON.parse(response);
+            console.log("xxx",data)
+            $('#preview').attr('src',data.data);
+        });
+    });
+}
+
+
 function initMsg(){
     $.ajax({
         url:"/chat/query?start=0&stop="+offset,
@@ -41,11 +72,21 @@ function getMsg(){
 
 
 function initappend(msg){
-    $("#chatbox li").first().before("<li><p>"+msg.time+"<p>"+msg.content+"</li>");
+    if (msg.img){
+        $("#chatbox li").first().before("<li><p>"+msg.time+"</p><p>"+msg.content+"</p><img class=\"msgimg\" src=\""+msg.img+"\"/></li>");
+    }else{
+        $("#chatbox li").first().before("<li><p>"+msg.time+"</p><p>"+msg.content+"</p></li>");
+    }
+    
 }
 
 function nextappend(msg){
-    $("#chatbox li").last().after("<li><p>"+msg.time+"<p>"+msg.content+"</li>");
+    if (msg.img){
+        $("#chatbox li").last().after("<li><p>"+msg.time+"</p><p>"+msg.content+"</p><img class=\"msgimg\" src=\""+msg.img+"\"/></li>");
+    }else{
+        $("#chatbox li").last().after("<li><p>"+msg.time+"</p><p>"+msg.content+"</p></li>");
+    }
+    
 }
 
 
@@ -63,6 +104,7 @@ function send(){
     }
     var msg = {
         content,
+        img:$('#preview').attr('src')
     }
     addMsg(msg);
     $("#cnt").val("");
@@ -114,4 +156,6 @@ $(function() {
     console.log("written by lsn");
     console.log("2017.08.28 love-day");
     setInterval(loop, 1000);
+
+    initimg();
 });
